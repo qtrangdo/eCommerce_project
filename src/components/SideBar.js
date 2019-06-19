23a1caefb5e8c3d2eb34760d99cebbe542b637de
Trang Do <quynhtrangdo94@gmail.com>
2019-06-19 12:09:53 -0400

    Created Categories component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import BasketCart from './BasketCart';
 import Search from './Search';
+import Categories from './Categories';
 
 const SideBar = () => (
   <div>
     <BasketCart />
     <Search />
+    <Categories />
   </div>
 )
 
