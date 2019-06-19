212cb417ca912b05e1b4a5343a05989e75814eaf
Trang Do <quynhtrangdo94@gmail.com>
2019-06-19 14:41:06 -0400

    Created Basket component

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Phones from './Phones';
 import Phone from './Phone';
+import Basket from './Basket';
 
 const App = ({ location }) => (
   <div>
     <Route path="/" location={location} exact component={Phones} />
     <Route path="/categories/:id" location={location} exact component={Phones} />
     <Route path="/phone/:id" location={location} exact component={Phone} />
+    <Route path="/basket" location={location} exact component={Basket} />
   </div>
 );
 
---
+++ src/components/Basket.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Basket = () => {
+  return (
+    <div>
+      Basket
+    </div>
+  )
+}
+
+export default Basket;
